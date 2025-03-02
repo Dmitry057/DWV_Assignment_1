39ebe689bf36fc66445dd8fb7f9fd713b447fa16
Dmitry057 <d.tetkin@innopolis.university>
2025-03-02 23:34:07 +0300

    update

--- notebooks/net_spider.py
+++ notebooks/net_spider.py
@@ -1,13 +1,16 @@
 import json
 import requests
-from bs4 import BeautifulSoup # parsing tool for wikipedia 
-import sqlite3 as sql # database tool
+from bs4 import BeautifulSoup # web scraping tool
+import sqlite3 as sql # for databases
 
-page_url = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films" # wikipedia page url
-base_wiki_url = "https://en.wikipedia.org" # base url for accessing additional pages
-response = requests.get(page_url) 
-soup = BeautifulSoup(response.text, "html.parser")
+targetWikiPage = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films" # wikipedia page url
+baseWikiPage = "https://en.wikipedia.org" # base url for accessing additional pages
 
+def get_soup(url: str) -> BeautifulSoup:
+    response = requests.get(url)
+    return BeautifulSoup(response.text, "html.parser")
+
+soup = get_soup(targetWikiPage)
 tables = soup.find_all("table", class_="wikitable")
 films_table = tables[0]
 
@@ -16,48 +19,32 @@ films = []
 
 # function to convert string representing revenue to int
 def str2int(string: str) -> int:
-    numeric = [char for char in string if char.isnumeric()]
-    return int("".join(numeric))
+    numeric = 
 
 # iterate through rows (except header row) and extract film data
 for row in rows[1:]:
     cells = row.find_all("td")
     headers = row.find_all("th")
-
     film_href = headers[0].find_all("a")[0]
-
+    
     # collect all available data from the row
     film = {
         "year": cells[3].text.strip(),
         "title": film_href.text.strip(),
-        "revenue": str2int(cells[2].text.strip()),
+        "revenue": int("".join([char for char in cells[2].text.strip() if char.isnumeric()])),
         "href": film_href["href"], # we will use this to access director and country data
     }
 
     films.append(film)
 films
 
-# We do a lot of consecutive requests, so we need to handle cases when the request fails
-def retry(max_retries: int = 3):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            retries = 0
-            while retries < max_retries:
-                try:
-                    return func(*args, **kwargs)
-                except Exception:
-                    retries += 1
-            raise Exception
 
-        return wrapper
-
-    return decorator
 
 
 # this function will parse film page and extract country data
 @retry()
 def get_country(href: str) -> str:
-    response = requests.get(base_wiki_url + href)
+    response = requests.get(baseWikiPage + href)
     soup = BeautifulSoup(response.text, "html.parser")
     data = soup.select_one("table", class_="infobox")
     try:
@@ -76,7 +63,7 @@ def get_country(href: str) -> str:
 # this function will parse film page and extract director data
 @retry()
 def get_director(href: str) -> str:
-    response = requests.get(base_wiki_url + href)
+    response = requests.get(baseWikiPage + href)
     soup = BeautifulSoup(response.text, "html.parser")
     data = soup.select_one("table", class_="infobox")
     director_td = data.find("th", string="Directed by").find_next("td")
